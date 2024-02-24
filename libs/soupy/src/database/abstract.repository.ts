98317b9b9bc2a618b063015c6a124b02774d8975
Izabelle Derrihu <izabelle.derrihu@idwall.co>
2024-02-23 23:35:32 -0300

    add: abstract repo and schema

---
+++ libs/soupy/src/database/abstract.repository.ts
@@ -0,0 +1,58 @@
+import { AbstractDocument } from "./abstract.schema";
+import { FilterQuery, Model, Types, UpdateQuery } from "mongoose";
+import { Logger, NotFoundException } from "@nestjs/common";
+
+export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+    protected abstract readonly logger: Logger;
+    constructor(protected readonly model: Model<TDocument>) {}
+
+    async create (document: Omit<TDocument, '_id'>): Promise<TDocument> {
+        const createdDocument = new this.model({
+            ...document,
+            _id: new Types.ObjectId(),
+        })
+
+        return (await createdDocument.save()).toJSON() as unknown as TDocument;
+    }
+
+    async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+        const document = await this.model.findOne(filterQuery).lean<TDocument>(true);
+
+        if (!document) {
+            this.logger.warn("Document was not found with filterQuery", filterQuery);
+            throw new NotFoundException('Document was not found')
+        }
+
+        return document;
+    }
+
+    async findOneAndUpdate(
+        filterQuery: FilterQuery<TDocument>,
+        update: UpdateQuery<TDocument>
+    ): Promise<TDocument> {
+        const document = this.model.findByIdAndUpdate(filterQuery, update, { new: true }).lean<TDocument>(true);
+        
+        if (!document) {
+            this.logger.warn("Document was not found with filterQuery", filterQuery);
+            throw new NotFoundException('Document was not found')
+        }
+
+        return document;
+    }
+
+    async find(filterQuery: FilterQuery<TDocument>): Promise<TDocument[]> {
+        const document = await this.model.find(filterQuery).lean<TDocument[]>(true);
+
+        if (document.length == 0) {
+            this.logger.warn("no Document was found with filterQuery", filterQuery);
+            throw new NotFoundException('no Document was found')
+        }
+
+        return document;
+    }
+
+    async findOneAndDelete(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+        return await this.model.findOneAndDelete(filterQuery).lean<TDocument>(true);
+    }
+
+}
