d576bcd34b43ef663eb30d91a227611537241afe
Izabelle Derrihu <izabelle.derrihu@idwall.co>
2024-02-25 00:11:24 -0300

    add: logger soupy module and validations

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
-import { DatabaseModule } from '@app/soupy';
+import { DatabaseModule, LoggerModule } from '@app/soupy';
 import { ReservationsRepository } from './reservations.repository';
 import { ReservationDocument, ReservationSchema } from './entities/reservation.schema';
-
 @Module({
-  imports: [DatabaseModule, DatabaseModule.forFeature([{ name: ReservationDocument.name, schema: ReservationSchema}])],
+  imports: [
+    DatabaseModule, 
+    DatabaseModule.forFeature([
+      { name: ReservationDocument.name, schema: ReservationSchema }
+    ]),
+    LoggerModule,
+  ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository]
 })
